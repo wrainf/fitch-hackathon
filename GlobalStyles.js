010531e0402edabe4a786cbc12dbb1a578697976
benben175 <benben1752002@gmail.com>
2023-11-02 16:39:00 +0000

    Set Global colours

---
+++ GlobalStyles.js
@@ -0,0 +1,5 @@
+export const GlobalRed = '#E82251';
+export const GlobalWhite = '#FFFFFF';
+export const BackgroundColor = '#000000';
+export const GlobalGrey = '#666666';
+export const GlobalLightGrey = '#F5F5F7';
