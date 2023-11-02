34882bc6be5f31fab3d3380b6aa3163557bcf8ce
wrainf <amirfaiz1806@gmail.com>
2023-11-02 19:26:31 +0000

    feat: basic working home page

--- App.jsx
+++ App.jsx
@@ -19,32 +19,8 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import Home from './src/Home';
 
-function Section({children, title}) {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -62,26 +38,7 @@ function App() {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+          <Home />
       </ScrollView>
     </SafeAreaView>
   );
---
+++ src/Home.jsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  useColorScheme,
+  Image,
+  Button,
+  View,
+  Dimensions,
+  ImageBackground,
+  TouchableOpacity
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import placeholder from '../assets/placeholder.jpg';
+import arrowLeft from '../assets/arrowLeft.png';
+import arrowRight from '../assets/arrowRight.png';
+
+
+
+
+function HomeEvent({title}) {
+  return (
+    <ImageBackground source={placeholder} style={styles.image}>
+      <View style={styles.sectionContainer}>
+        <Text
+          style={styles.sectionTitle}>
+          {title}
+        </Text>
+        <Button
+          title="Learn more"
+          style={styles.sectionButton}
+        />
+      </View>
+    </ImageBackground>
+    
+  );
+}
+
+
+function Home() {
+  const height = Dimensions.get('window').height;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const events = ['2022 Traditional Christmas Party', '2023 Traditional Christmas Party', '2024 Traditional Christmas Party'];
+
+
+
+  const goToPreviousEvent = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + events.length) % events.length);
+  };
+
+  const goToNextEvent = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % events.length);
+  };
+
+  return (
+    <View style={{ height: height }}>
+      <Text style={{ fontSize: 24, zIndex: 1, color: '#E82251', fontWeight: '700', marginTop: 28, marginLeft: 20 }}>
+          Upcoming Events
+        </Text>
+        <HomeEvent title={events[currentIndex]} />
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 20, position: 'absolute', top: '50%', width: '100%' }}>
+          <TouchableOpacity onPress={goToPreviousEvent}>
+            <Image source={arrowLeft}/>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={goToNextEvent}>
+            <Image source={arrowRight} />
+          </TouchableOpacity>
+        </View>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 60,
+    backgroundColor: 'rgba(0,0,0,0.7)'
+  },
+  sectionTitle: {
+    fontSize: 36,
+    fontWeight: '600',
+    color: '#f5f5f7',
+    maxWidth: '65%',
+    textAlign: 'center',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  sectionButton: {
+    padding: 20,
+  },
+  image: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%'
+  }
+});
+
+export default Home;
